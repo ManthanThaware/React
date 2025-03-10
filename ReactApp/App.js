0360631189a998b3d2ce26c482d48aa2d5f12b41
Manthan Thaware <manthanthaware@gmail.com>
2025-03-10 14:59:20 +0530

    Till1003

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,7 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const num = 10; 
+const Demo =() => {
+    return(
+   <div > 
+    {/* <div id="parent" style ={{color:"red"}}>
+        <div id="child">
+          <h1 >Heading1</h1>
+          <h2>Heading2</h2>
+        </div>
+      </div>  */}
 
-const demo = React.createElement("H1",{},"Hello");
+       <div id="parent">
+        <div id="child"style ={{color:"red"}}>
+          <h1>Heading1</h1>
+          <h2>Heading2</h2>
+        </div>
+        <div id="child-1">
+          <h1>Heading1</h1>
+          <h2>Heading2 {num}</h2>
+        </div>
+      </div> 
+      </div>
+    );
+ };
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(demo);
+root.render(<Demo/>);
